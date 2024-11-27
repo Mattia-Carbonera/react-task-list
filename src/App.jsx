576e54a666d7fde60fa04ad0603fed06b3595d91
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-27 14:24:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+// import tasks array
+import tasks from "../db/tasks";
+
+function App() {
+  return <>{/* code */}</>;
+}
+
+export default App;
